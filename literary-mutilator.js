5bf349068b254467419e7253048145a43fcf7424
jmurph1992 <j.murphyjt@gmail.com>
2019-08-08 22:34:44 -0600

    added a few new functions like party box

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -2,34 +2,43 @@ window.onload = function () {
 	colorChange();
 	fontChange();
 	highlight();
-	wordsToArray();
-};
+	partyBoxDown();
+	partyBoxUp();
+	};
 
 function colorChange() {
-//changes the color of the paragraphs
+//new colors!
 	document.getElementById("first").style.color = "green";
 	document.getElementById("second").style.color = "purple";
 }
 
 function fontChange() {
-//changes the font style of the paragraphs
+//new fonts!
 	document.getElementById("first").style.fontFamily = "Impact,sans-serif";
 	document.getElementById("second").style.fontFamily = "Courier New";
 }
 
 function highlight() {
-	//highlights the paragraphs
+	//highlights the paragraphs on load (took me a while to figure this out. There is probably an easier way.)
 	var range = document.createRange();
 	var selection = window.getSelection();
-	range.selectNodeContents(document.getElementById('paragraphs'));	
+	range.selectNodeContents(document.getElementById('paragraphs'));
 
 	selection.removeAllRanges();
 	selection.addRange(range);
 }
 
-function wordsToArray() {
-
-	let paragraph = "This is a string.";
-	let words = paragraph.split(' ');
-	console.log(words[3]);
+function partyBoxDown() {
+	//click those keys
+	document.getElementById("third").style.backgroundColor =  "yellow";
+	console.log("WOOOOO PAAARRRTYYYYY!!!!");
 }
+
+function partyBoxUp() {
+	//click those keys
+	document.getElementById("third").style.backgroundColor = "magenta";
+}
+
+window.addEventListener("keydown", function(){partyBoxDown()} );
+window.addEventListener("keyup", function(){partyBoxUp()} );
+
